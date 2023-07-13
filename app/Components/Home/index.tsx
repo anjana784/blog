d003af82393d42492f674f34b3fa49682064d62b
anjana784 <hello@anjana784.dev>
2023-07-14 02:15:55 +0530

    centered content

--- app/Components/Home/index.tsx
+++ app/Components/Home/index.tsx
@@ -19,7 +19,7 @@ const Home: FC = () => {
       <div className="w-full h-[600px] 2xl:h-[600px] xl:h-[620px] lg:h-[650px] md:h-[700px] sm:h-[750px] xsm:h-[800px] flex justify-center items-center">
         <div className="w-full h-[600px] 2xl:h-[600px] xl:h-[620px] lg:h-[650px] md:h-[700px] sm:h-[750px] xsm:h-[800px]">
           <div className="w-full h-full flex 2xl:flex 2xl:flex-row 2xl:items-center xl:flex xl:flex-row xl:items-center lg:flex lg:flex-row lg:items-center md:flex-col sm:flex-col xsm:flex-col">
-            <div className="w-full h-full 2xl:w-1/2 xl:w-1/2 lg:w-1/2 flex items-center 2xl:items-center xl:items-center lg:items-center md:items-start sm:items-start xsm:items-start">
+            <div className="w-full h-full 2xl:w-1/2 xl:w-1/2 lg:w-1/2 flex items-center">
               <div>
                 <h1 className="text-primaryGold text-4xl mt-0 2xl:text-4xl 2xl:mt-0 xl:text-3xl xl:mt-0 lg:text-2xl lg:mt-0 md:text-xl md:mt-8 sm:text-lg sm:mt-8 xsm:text-lg xsm:mt-8">
                   <TypeAnimation
@@ -42,9 +42,10 @@ const Home: FC = () => {
                     animate={{ opacity: 1 }}
                     transition={{ duration: 1, delay: 0.5 }}
                   >
-                    A Professional Full-Stack Developer Experienced in Trending
-                    JavaScript Technologies like Next.js, MERN Stack, and React
-                    Native. Curious to Know More? Jump into the About Section.
+                    A Professional Full-Stack Web and Mobile Application
+                    Developer, Experienced in Trending and Latest JavaScript
+                    Technologies. Curious to Know More? Jump into the About
+                    Section.
                   </motion.h2>
                 )}
                 {/* <div className="h-16 flex justify-between mt-2 w-2/5">
