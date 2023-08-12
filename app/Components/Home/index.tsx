09c3fb08d21ce3929dd44e5e75d7e06d858caba5
anjana784 <hello@anjana784.dev>
2023-08-12 20:14:26 +0530

    changed transition

--- app/Components/Home/index.tsx
+++ app/Components/Home/index.tsx
@@ -41,7 +41,7 @@ const Home: FC = () => {
                       className="text-primaryWhite mt-4 w-full 2xl:w-full xl:w-full lg:w-full md:w-4/5 sm:w-4/5 xsm:w-4/5 text-lg 2xl:text-lg xl:text-lg lg:text-base md:text-sm sm:text-xs xsm:text-xs"
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
-                      transition={{ duration: 2, delay: 1 }}
+                      transition={{ duration: 2, delay: 0 }}
                     >
                       A Professional Full-Stack Web and Mobile Application
                       Developer, Experienced in Trending and Latest JavaScript
@@ -52,7 +52,7 @@ const Home: FC = () => {
                       Section.
                     </motion.h2>
                     <motion.div
-                      className="h-16 flex justify-between mt-8 w-2/5 2xl:w-2/5 xl:w-2/5 lg:w-2/5 md:w-1/3 sm:w-1/3 xsm:w-1/2"
+                      className="h-16 flex justify-between mt-8 w-56 2xl:w-56 xl:w-64 lg:w-64 md:w-56 sm:w-48 xsm:w-40"
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       transition={{ duration: 2, delay: 1 }}
